42b17a03caa6a47e92e7497abd4fc480dd83c786
Jacobo Rodriguez <2797100-Jacobo9810499@users.noreply.replit.com>
2024-05-25 21:55:11 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+ //estructuras de datos
+/**
+ * !--- tasks Queue ---! -> se usa para callbacks
+ * est tipo fifo
+ * es un objeto que hace referencia a:
+ * 1) info resultado
+ * 2) acciones cinsecuencia
+ *
+ *!--- micro tasks queue ---! -> se usa para 
+ *
+ * !--- event loop ---! -> es la encargada de ejecutar las tareas que se encuentran en el call stack y extrae los objetos de task queue y microtasks queue y los ejecuta, las tareas del microtasks tiene prioridad sobre las del call queue esto se llama event loop
+ *
+ *
+ * 
+ */

46d9d4b818674155959aa21201e9c53596069797
Jacob <jacobo.rodriguez499@gmail.com>
2024-05-25 22:56:47 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -9,7 +9,17 @@
  *!--- micro tasks queue ---! -> se usa para 
  *
  * !--- event loop ---! -> es la encargada de ejecutar las tareas que se encuentran en el call stack y extrae los objetos de task queue y microtasks queue y los ejecuta, las tareas del microtasks tiene prioridad sobre las del call queue esto se llama event loop
+ * !--- ciclo de vida de acción asincrona ---!
+ * 1) cakk stack
+ * 2) levanta hilo de Sistema operativo
+ * 3) el hilo termina y emite un evento
+ * 4) se pasa el obj resultado al event queue
+ * 5) cuando el call stack esté vacio, event loop  toma acciones consecuencia y ejectua
+ * hay foto
  *
- *
- * 
+ * me canismos de ma
  */
+
+const anyFction =() =>{
+  setTimeout(()=> // accion asincron a revisar el git de leon para ver el codigo completo 
+}
